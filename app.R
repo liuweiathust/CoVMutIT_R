4b27a773509842075dbf4ebd3af64c44f9e0b39c
liuweiathust <liuweiathust@foxmail.com>
2021-09-08 15:53:52 +0800

    Load packages and data for geomap plot

--- app.R
+++ app.R
@@ -20,6 +20,9 @@ library(patchwork)
 library(ggrepel)
 library(leaflet)
 library(jsonlite)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
 
 # Set ggplot2 default theme -----------------------------------------------
@@ -460,6 +463,8 @@ mutations_monthly_count_all <- readr::read_rds("data/mutations_monthly_count_tab
 mutations_monthly_count_each <- readr::read_rds("data/mutations_monthly_count_table_each.rds")
 mutation_position_table <- readr::read_rds("data/mutation_position_table.rds")
 country_code_table <- readr::read_rds("data/country_code_table.rds")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
 
 # -------------------------------------------------------------------------
 
