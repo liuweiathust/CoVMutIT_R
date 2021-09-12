c30e5945469e556bb27b9254ce54039830959f5a
liuweiathust <liuweiathust@foxmail.com>
2021-09-12 22:24:26 +0800

    Encapsulate the predict method into a function

--- predict.R
+++ predict.R
@@ -8,16 +8,16 @@ suppressPackageStartupMessages(library(patchwork))
 suppressPackageStartupMessages(library(ggnewscale))
 suppressPackageStartupMessages(library(bignum))
 
-println <- function(line) {cat(line); cat("\n")}
-
-mutation_freq_table <- readr::read_rds("data/predict_example_data.rds")
-
-colnames(mutation_freq_table) <- c("mutation", "position", "freq_prev", "freq_next")
-
-#%% subsample 1% of the example data to accelerate the prediction process
-mutation_freq_table <- mutation_freq_table[sample(nrow(mutation_freq_table), ceiling(nrow(mutation_freq_table) / 10)), ]
-
-mutation_freq_table %<>% filter(freq_prev != 0)
+# println <- function(line) {cat(line); cat("\n")}
+# 
+# mutation_freq_table <- readr::read_rds("data/predict_example_data.rds")
+# 
+# colnames(mutation_freq_table) <- c("mutation", "position", "freq_prev", "freq_next")
+# 
+# #%% subsample 1% of the example data to accelerate the prediction process
+# mutation_freq_table <- mutation_freq_table[sample(nrow(mutation_freq_table), ceiling(nrow(mutation_freq_table) / 10)), ]
+# 
+# mutation_freq_table %<>% filter(freq_prev != 0)
 
 #%%
 BN_target_score_fun <- function(freq_table, F) {
@@ -55,82 +55,98 @@ generate_shrink_factor_for_bignum_score <- function(score) {
 # 
 target_func_label <- latex2exp::TeX('$target = \\prod(dbeta(P_{b},   \\frac{1 - F}{F} \\cdot P_{a},   \\frac{1 - F}{F} \\cdot (1 - P_{a}) ))$')
 
+covmutit_predict <- function(mutation_freq_table) {
 
-
-x1 <- seq(1e-5, 1, length=1000)
-y1 <- rep(0, length(x1))
-z1 <- rep(0, length(x1))
-
-for (i in seq_along(x1)) {
-  F <- x1[i]
-  y1[i] <- BN_target_score_fun(freq_table=mutation_freq_table, F=F)
+  # 1st iteration ---------------------------------------------------------------------------------------------------
+  x1 <- seq(1e-5, 1, length=1000)
+  y1 <- rep(0, length(x1))
+  z1 <- rep(0, length(x1))
+  
+  for (i in seq_along(x1)) {
+    F <- x1[i]
+    y1[i] <- BN_target_score_fun(freq_table=mutation_freq_table, F=F)
+  }
+  
+  F1 <- x1[get_max_bigfloat_index(y1)]
+  max_score1 <- BN_target_score_fun(freq_table=mutation_freq_table, F=F1)
+  
+  shrink_factor1 <- generate_shrink_factor_for_bignum_score(max_score1)
+  
+  for (i in seq_along(x1)) {
+    z1[i] <- as.numeric(bignum::as_bigfloat(y1[i]) / bignum::as_bigfloat(shrink_factor1))
+  }
+  
+  
+  # 2nd iteration ---------------------------------------------------------------------------------------------------
+  x2 <- seq(max(F1-0.0005, 1e-6), min(F1+0.0005, 1), length=1000)
+  y2 <- rep(0, length(x2))
+  z2 <- rep(0, length(x2))
+  
+  for (i in seq_along(x2)) {
+    F = x2[i]
+    y2[i] <- BN_target_score_fun(freq_table=mutation_freq_table, F=F)
+  }
+  
+  F2 <- x2[get_max_bigfloat_index(y2)]
+  
+  max_score2 <- BN_target_score_fun(freq_table=mutation_freq_table, F=F2)
+  
+  shrink_factor2 <- generate_shrink_factor_for_bignum_score(max_score2)
+  
+  for (i in seq_along(x2)) {
+    z2[i] <- as.numeric(bignum::as_bigfloat(y2[i]) / bignum::as_bigfloat(shrink_factor2))
+  }
+  
+  F <- F2
+  
+  p1 <- tibble(x = x1, y = z1) %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    scale_y_continuous(breaks = NULL) +
+    ylab(target_func_label) +
+    xlab("F") +
+    theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  
+  p2 <- tibble(x = x2, y = z2) %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_line() +
+    scale_y_continuous(breaks = NULL) +
+    scale_x_continuous(labels = function(x) format(x, scientific = TRUE)) +
+    geom_vline(xintercept = F, size = 0.3, color = "gray", linetype = "dashed") +
+    ylab("") +
+    xlab("") +
+    annotation_custom(
+      grid::textGrob(paste("F", "=", signif(F, 3), sep = " "), gp = grid::gpar(col = "steelblue", fontsize = 9)),
+      xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf
+    ) +
+    theme_bw() +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      axis.text.x = element_text(angle = 45, vjust=0.5, size=8),
+    )
+  
+  F_estimate_plot <- ggdraw(p1) + draw_plot(p2 , 0.47, 0.47, 0.5, 0.5)
+  
+  pvalue_table <- mutation_freq_table %>%
+    filter(freq_prev != 0) %>%
+    mutate(shape1=(1-F)/F*freq_prev, shape2=(1-F)/F*(1-freq_prev)) %>%
+    mutate(pvalue=1-pbeta(freq_next, shape1, shape2)) %>%
+    arrange(pvalue)
+  
+  freq_scatter_plot <- ggplot() +
+    geom_abline(slope=1, intercept = 0, color="#CCCCCC", linetype=2, size=1) +
+    geom_point(aes(x=freq_prev, y=freq_next), data=pvalue_table, size=2, alpha=0.8, color="#999999") +
+    scale_x_continuous(labels = scales::percent, limits = c(0, 1)) +
+    scale_y_continuous(labels = scales::percent, limits = c(0, 1)) +
+    xlab("mutation frequency (prev-month)") +
+    ylab("mutation frequency (next-month)") +
+    coord_fixed() 
+    
+  
+  return(list(F_estimate_plot=F_estimate_plot, table=pvalue_table, scatter_plot=freq_scatter_plot))
 }
 
-F1 <- x1[get_max_bigfloat_index(y1)]
-max_score1 <- BN_target_score_fun(freq_table=mutation_freq_table, F=F1)
 
-shrink_factor1 <- generate_shrink_factor_for_bignum_score(max_score1)
-
-for (i in seq_along(x1)) {
-  z1[i] <- as.numeric(bignum::as_bigfloat(y1[i]) / bignum::as_bigfloat(shrink_factor1))
-}
-
-x2 <- seq(max(F1-0.0005, 1e-6), min(F1+0.0005, 1), length=1000)
-y2 <- rep(0, length(x2))
-z2 <- rep(0, length(x2))
-
-for (i in seq_along(x2)) {
-  F = x2[i]
-  y2[i] <- BN_target_score_fun(freq_table=mutation_freq_table, F=F)
-}
-
-F2 <- x2[get_max_bigfloat_index(y2)]
-
-max_score2 <- BN_target_score_fun(freq_table=mutation_freq_table, F=F2)
-
-shrink_factor2 <- generate_shrink_factor_for_bignum_score(max_score2)
-
-for (i in seq_along(x2)) {
-  z2[i] <- as.numeric(bignum::as_bigfloat(y2[i]) / bignum::as_bigfloat(shrink_factor2))
-}
-
-F <- F2
-
-p1 <- tibble(x = x1, y = z1) %>%
-  ggplot(aes(x = x, y = y)) +
-  geom_line() +
-  scale_y_continuous(breaks = NULL) +
-  ylab(target_func_label) +
-  xlab("F") +
-  theme_bw() +
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-
-p2 <- tibble(x = x2, y = z2) %>%
-  ggplot(aes(x = x, y = y)) +
-  geom_line() +
-  scale_y_continuous(breaks = NULL) +
-  scale_x_continuous(labels = function(x) format(x, scientific = TRUE)) +
-  geom_vline(xintercept = F, size = 0.3, color = "gray", linetype = "dashed") +
-  ylab("") +
-  xlab("") +
-  annotation_custom(
-    grid::textGrob(paste("F", "=", signif(F, 3), sep = " "), gp = grid::gpar(col = "steelblue", fontsize = 9)),
-    xmin = -Inf, xmax = Inf, ymin = -Inf, ymax = Inf
-  ) +
-  theme_bw() +
-  theme(
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    axis.text.x = element_text(angle = 45, vjust=0.5, size=8),
-  )
-
-ggdraw(p1) + draw_plot(p2 , 0.47, 0.47, 0.5, 0.5)
-
-bn_F_estimate_plot <- ggdraw(p1) + draw_plot(p2 , 0.47, 0.47, 0.5, 0.5)
-
-mutation_freq_table %>%
-  filter(freq_prev != 0) %>%
-  mutate(shape1=(1-F)/F*freq_prev, shape2=(1-F)/F*(1-freq_prev)) %>%
-  mutate(pvalue=1-pbeta(freq_next, shape1, shape2)) %>%
-  arrange(pvalue)
 
